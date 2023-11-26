a1cc72d2ef8541ea6ba93bb091f51ff82794ac3f
aditya39p <adityavsec@gmail.com>
2023-11-25 18:13:05 -0800

    Uploaded Test

--- My project (4)/Assets/Test/playMode/numberTest.cs
+++ My project (4)/Assets/Test/playMode/numberTest.cs
@@ -30,4 +30,78 @@ public class numberTest
         }
         Assert.AreEqual(true, success);
     }
+
+    [UnityTest]
+    public IEnumerator Initial_Draw_test()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return new WaitForSeconds(7);
+
+       GameObject x = GameObject.Find("hand");
+       bool y = false;
+       Debug.Log(x.transform.childCount);
+       if(x.transform.childCount==5){
+        y = true;
+       }
+       Assert.AreEqual(true, y);
+    }
+
+    [UnityTest]
+
+    public IEnumerator turn_test(){
+
+    yield return new WaitForSeconds(7);
+    GameObject gameObject = new GameObject();
+    turnScript turnScriptComponent = gameObject.AddComponent<turnScript>();
+
+    turnScriptComponent.Start();
+
+    // Ensure initial values are set correctly
+    Debug.Log("Initial Values:");
+    Debug.Log("isMyTurn: " + turnScriptComponent.isMyTurn);
+    Debug.Log("myTurn: " + turnScriptComponent.myTurn);
+    Debug.Log("isTheirTurn: " + turnScriptComponent.isTheirTurn);
+    Debug.Log("maxMana: " + turnScriptComponent.maxMana);
+    Debug.Log("currentMana: " + turnScriptComponent.currentMana);
+    Debug.Log("turnStart: " + turnScript.turnStart);
+
+    Assert.IsTrue(turnScriptComponent.isMyTurn);
+    Assert.AreEqual(1, turnScriptComponent.myTurn);
+    Assert.AreEqual(0, turnScriptComponent.isTheirTurn);
+    Assert.AreEqual(1, turnScriptComponent.maxMana);
+    Assert.AreEqual(1, turnScriptComponent.currentMana);
+    Assert.IsFalse(turnScript.turnStart);
+
+    // Simulate ending player's turn
+    turnScriptComponent.endTurn();
+    
+
+    Debug.Log("After ending player's turn:");
+    Debug.Log("isMyTurn: " + turnScriptComponent.isMyTurn);
+    Debug.Log("isTheirTurn: " + turnScriptComponent.isTheirTurn);
+
+    // Ensure it's now opponent's turn
+    Assert.IsFalse(turnScriptComponent.isMyTurn);
+    Assert.AreEqual(1, turnScriptComponent.isTheirTurn);
+
+    // Simulate ending opponent's turn
+    turnScriptComponent.endOpponentTurn();
+    yield return null; // Wait for the end of frame update
+
+    Debug.Log("After ending opponent's turn:");
+    Debug.Log("isMyTurn: " + turnScriptComponent.isMyTurn);
+    Debug.Log("myTurn: " + turnScriptComponent.myTurn);
+    Debug.Log("maxMana: " + turnScriptComponent.maxMana);
+    Debug.Log("currentMana: " + turnScriptComponent.currentMana);
+    Debug.Log("turnStart: " + turnScript.turnStart);
+
+    // Ensure it's back to player's turn, and mana is updated
+    Assert.IsTrue(turnScriptComponent.isMyTurn);
+    Assert.AreEqual(1, turnScriptComponent.myTurn);
+    Assert.AreEqual(2, turnScriptComponent.maxMana);
+    Assert.AreEqual(2, turnScriptComponent.currentMana);
+    Assert.IsTrue(turnScript.turnStart); 
+}
+
 }
